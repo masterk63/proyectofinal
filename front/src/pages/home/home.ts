3d991213bba9e26140c6584f32fc5b0ceb66a679
MasterK <masterk@macpro.local>
2017-02-08 18:37:33 -0300

    test tutorial

--- front/src/pages/home/home.ts
+++ front/src/pages/home/home.ts
@@ -1,63 +1,155 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import {Camera} from "ionic-native";
-import { Transfer } from 'ionic-native';
+// import {NavController} from 'ionic-angular';
+// import {Camera} from "ionic-native";
+// import { Transfer } from 'ionic-native';
 
- 
+
+import { NavController, ActionSheetController, ToastController, Platform, LoadingController, Loading } from 'ionic-angular';
+import { Camera, File, Transfer, FilePath } from 'ionic-native';
+
+declare var cordova: any;
+
 @Component({
     templateUrl: 'home.html'
 })
+
 export class HomePage {
  
     public base64Image: string;
+    lastImage: string = null;
+  loading: Loading;
+
+    // constructor(private navController: NavController) {
+    //     this.base64Image = "https://placehold.it/150x150";
+    // }
+    
+    constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController) {}
+
+    public presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(Camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(Camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+public takePicture(sourceType) {
+  // Create options for the Camera Dialog
+  var options = {
+    quality: 100,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false,
+    correctOrientation: true
+  };
  
-    constructor(private navController: NavController) {
-        this.base64Image = "https://placehold.it/150x150";
+  // Get the data of an image
+  Camera.getPicture(options).then((imagePath) => {
+    // Special handling for Android library
+    if (this.platform.is('android') && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+      FilePath.resolveNativePath(imagePath)
+      .then(filePath => {
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = filePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      });
+    } else {
+      var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
     }
- 
-    public takePicture() {
-        console.log('antes de llamar a la funcion');
-        Camera.getPicture({ 
-            quality : 75,
-            destinationType : Camera.DestinationType.DATA_URL,
-            sourceType : Camera.PictureSourceType.CAMERA,
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            saveToPhotoAlbum: false
-        }).then(imageData => {
-            this.base64Image = "data:image/jpeg;base64," + imageData;
-            
-            this.subirFoto(this.base64Image);
-             console.log('despues de llamar a la funcion');
-        }, error => {
-            console.log("ERROR -> " + JSON.stringify(error)); 
-        });
-    }
-
- 
-
-
-public subirFoto(urlfoto){
-  const fileTransfer = new Transfer();
-  var options: any;
-
-  options = {
-    fileKey: 'file',
-    fileName: 'name.jpg',
-    chunkedMode: false,
-    mimeType: "image/png"
-  } 
-  fileTransfer.upload("urlfoto", "http://rickybruno.sytes.net/proyectofinal/back/public/subirfoto", options)
-   .then((data) => {
-     // success
-      console.log("SUCCESS: " + JSON.stringify(data));
-   }, (err) => {
-     // error
-     console.log("ERROR: " + JSON.stringify(err));
-   })
+  }, (err) => {
+    this.presentToast('Error while selecting image.');
+  });
 }
+
+// Create a new name for the image
+private createFileName() {
+  var d = new Date(),
+  n = d.getTime(),
+  newFileName =  n + ".jpg";
+  return newFileName;
+}
+ 
+// Copy the image to a local folder
+private copyFileToLocalDir(namePath, currentName, newFileName) {
+  File.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.lastImage = newFileName;
+  }, error => {
+    this.presentToast('Error while storing file.');
+  });
+}
+ 
+private presentToast(text) {
+  let toast = this.toastCtrl.create({
+    message: text,
+    duration: 3000,
+    position: 'top'
+  });
+  toast.present();
+}
+ 
+// Always get the accurate path to your apps folder
+public pathForImage(img) {
+  if (img === null) {
+    return '';
+  } else {
+    return cordova.file.dataDirectory + img;
+  }
+} 
+
+
+public uploadImage() {
+  // Destination URL
+  var url = "http://rickybruno.sytes.net/proyectofinal/back/public/subirfoto";
+ 
+  // File for Upload
+  var targetPath = this.pathForImage(this.lastImage);
+ 
+  // File name only
+  var filename = this.lastImage;
+ 
+  var options = {
+    fileKey: "file",
+    fileName: filename,
+    chunkedMode: false,
+    mimeType: "multipart/form-data",
+    params : {'fileName': filename}
+  };
+ 
+  const fileTransfer = new Transfer();
+ 
+  this.loading = this.loadingCtrl.create({
+    content: 'Uploading...',
+  });
+  this.loading.present();
+ 
+  // Use the FileTransfer to upload the image
+  fileTransfer.upload(targetPath, url, options).then(data => {
+    this.loading.dismissAll()
+    this.presentToast('Image succesful uploaded.');
+  }, err => {
+    this.loading.dismissAll()
+    this.presentToast('Error while uploading file.');
+  });
+}
+
     
  
 }
