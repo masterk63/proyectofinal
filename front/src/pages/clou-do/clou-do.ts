50ff8a864e0bba22ace7ed930ffd27a5232bcfb1
Kevin <MasterK@MacBookPro-MasterK.local>
2017-02-17 19:05:11 -0300

    live reload working with Observable, Subscribe and Ngzone

--- front/src/pages/clou-do/clou-do.ts
+++ front/src/pages/clou-do/clou-do.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { Todos } from '../../providers/todos';
@@ -14,11 +14,13 @@ export class ClouDOPage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public todoService: Todos, 
-              public alertCtrl: AlertController) {}
+              public alertCtrl: AlertController,
+              private _zone: NgZone) {}
 
   ionViewDidLoad() {
-     this.todoService.getTodos().then((data) => {
-      this.todos = data;
+    //Zone Run Refresaca la pagina
+     this.todoService.getTodos().subscribe((data) => {
+      this._zone.run(() => this.todos = data);
     });
   }
 
