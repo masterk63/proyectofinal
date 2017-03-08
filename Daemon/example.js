23eda28b2e2ad6d58e0dd33c95760df3a256e6be
MasterK <masterk@macpro.local>
2017-03-08 14:58:56 -0300

    working upload img mysql

--- Daemon/example.js
+++ Daemon/example.js
@@ -11,35 +11,44 @@ function foto1(self,change,fn){
     });
 }
 
+function foto2(self,change,fotoMuestra,fn){
+    self.database.attachment.get(change.id, "fotoPaisaje.png", function(err, body) {
+        if (err) throw err;
+        fn(body.toString('base64'));
+    });
+}
+
 cvr.on('created', function (change) {
     console.log('cambioDetectado');
     var self = this;
     var query = this.config.queries.insert;
     
-    // foto1(self,change,function(hola)
-    
-    this.database.get(change.id, function (err, res) {
-        if (err) throw err;
-        
-        //Aca esta la magia modificar doc para lo que se necesario.
-        var doc = { 
-            idRegistro : 2,
-            latitud: 65.4545,
-            longitud:-55.24,
-            fotoPaisaje: res.fotoPaisaje,
-            fotoMuestra: res.fotoMuestra,
-            observacion: 'test de comentario',
-            valido:1,
-            pendiente:0,
-            idUsuario : 1,
-            idUbicacion: 1,
-        };
-        // self.mysql.query(query, doc, function (err) {
-        //     if(err){
-        //         console.log(err);
-        //     }
+    foto1(self,change,function(fotoMuestrabd){
+        foto2(self,change,fotoMuestrabd,function(fotoPaisajebd){
+            self.database.get(change.id, function (err, res) {
+            if (err) throw err;
             
-        // });
+            //Aca esta la magia modificar doc para lo que se necesario.
+            var doc = { 
+                idRegistro : 2,
+                latitud: 65.4545,
+                longitud:-55.24,
+                fotoPaisaje: fotoPaisajebd,
+                fotoMuestra: fotoMuestrabd,
+                observacion: 'test de comentario',
+                valido:1,
+                pendiente:0,
+                idUsuario : 1,
+                idUbicacion: 1,
+            };
+            self.mysql.query(query, doc, function (err) {
+                if(err){
+                    console.log(err);
+                }
+                
+            });
+    });
+        });
     });
 });
 
