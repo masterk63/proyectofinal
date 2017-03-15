48ecb3b720090f87995f1973cc31f7eb36bd04c6
Kevin <MasterK@MacBookPro-MasterK.local>
2017-03-15 08:31:50 -0300

    trabajando con pouch

--- front/src/pages/mis-registros/mis-registros.ts
+++ front/src/pages/mis-registros/mis-registros.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Localsave } from '../../providers/localsave';
+import {File, Transfer, FilePath } from 'ionic-native';
 
 @Component({
   selector: 'page-mis-registros',
