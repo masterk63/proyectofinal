057dd127a05658e9676496ada5a750a56d6dea27
Gomez <kevin.gomez@sovos.com>
2017-12-29 11:30:34 -0300

    trabajando en el webSocket

--- passport-couchdb-master/webSocket.js
+++ passport-couchdb-master/webSocket.js
@@ -6,8 +6,16 @@ module.exports = function (server) {
 
     socket.on('connection', function (connection) {
         console.log('Se conecto un nuevo usuario');
-        connection.on('message', function (msg) {
-            socket.emit('message', msg);
+        connection.emit('handShake', { hello: 'Bienvenido' });
+        connection.on('crearRoom', function (data) {
+            console.log('creando Sala')
+            connection.join(data.user);
         });
+
+        connection.on('enviarInfo', function (data) {
+            console.log('enviando info a quien corresponda')
+            socket.to(data.user).emit('mensaje');
+        });
+        
     });
 }
