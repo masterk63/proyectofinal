39cc59363b88f129771df93dda209f7073fa8a46
Kevin Shionen Gomez Veliz <masterk@MacBook-Pro-de-Kevin.local>
2017-12-29 19:14:58 -0300

    agragando socket

--- passport-couchdb-master/webSocket.js
+++ passport-couchdb-master/webSocket.js
@@ -9,13 +9,15 @@ module.exports = function (server) {
         connection.emit('handShake', { hello: 'Bienvenido' });
         connection.on('crearRoom', function (data) {
             console.log('creando Sala')
+            console.log('se unio el usuario',data.user);
             connection.join(data.user);
         });
 
         connection.on('enviarInfo', function (data) {
-            console.log('enviando info a quien corresponda')
-            socket.to(data.user).emit('mensaje');
+            console.log('enviando info a quien corresponda, en este caso',data)
+            socket.to(data.user).emit('mensaje',{ mensaje: 'esto es solo para mi' });
         });
+
         
     });
 }
