ad9ee14a1b661a5a6c105d183f317842d1693431
Kevin Shionen Gomez Veliz <masterk@MacBook-Pro-MasterK.local>
2018-03-27 19:54:55 -0300

    styling

--- aguita/src/server.ts
+++ aguita/src/server.ts
@@ -1,6 +1,6 @@
 export let data = {
     // "urlServidor": 'http://rickybruno.sytes.net:3000',
     // "urlServidor": 'http://localhost:3000',
-    "urlServidor": 'http://192.168.0.13:3000',
+    "urlServidor": 'http://192.168.1.16:3000', 
     "password": "false" 
 };
