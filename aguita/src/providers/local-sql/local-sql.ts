1235112e0e7220fd6624e62285a5abaa432f2546
Kevin Shionen Gomez Veliz <masterk@MacBook-Pro-MasterK.local>
2018-06-24 12:38:55 -0300

    mierda

--- aguita/src/providers/local-sql/local-sql.ts
+++ aguita/src/providers/local-sql/local-sql.ts
@@ -181,11 +181,7 @@ export class LocalSqlProvider {
 
   fakeRegistro() {
     return Observable.create(observer => {
-<<<<<<< HEAD
       let sql = 'INSERT INTO tasks(indice, fecha, latitud, longitud,latitudFoto,longitudFoto, fotoPaisaje,fotoMuestra,fotoMapa, observacion, elmido, patudo, plecoptero, tricoptero, idUsuario) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
-=======
-      let sql = 'INSERT INTO tasks(indice, fecha, latitud, longitud,fotoPaisaje,fotoMuestra,fotoMapa, observacion, elmido, patudo, plecoptero, tricoptero, idUsuario) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)';
->>>>>>> 64c9affde3bfc6db25f0ea371ee74cc09ce39f6a
       this.db.executeSql(sql, [3, '2017/12/25', -26.81, -65.25, -26.81, -65.25, 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'hola', 'si', 'si', 'si', 'si', 2]).then(res => {
         console.log('registro agregado con exito')
         this.dame(res.insertId).then((reg) => {
