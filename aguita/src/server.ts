2639932a44498d24d1b605175d412eeee101e2d1
rickybruno89 <ricardobruno_89@hotmail.com>
2018-09-12 21:10:09 -0300

    fix working server ingeit socket io

--- aguita/src/server.ts
+++ aguita/src/server.ts
@@ -1,7 +1,7 @@
 export let data = {
     // "urlServidor": 'http://rickybruno.sytes.net:3000',
     // "urlServidor": 'http://rickybruno.ddns.net:3000',
-    // "urlServidor": 'http://localhost:3000', 
-    "urlServidor": 'https://ingeit.ddns.net/aguita/api/', 
+    // "urlServidor": 'http://192.168.1.23:3000', 
+    "urlServidor": 'https://ingeit.ddns.net/aguita', 
     "password": "false" 
 };
