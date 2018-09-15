aa2e55d9faf4a58839c5522bd1ef26d0583f4553
Kevin Shionen Gomez Veliz <masterk@MacBook-Pro-MasterK.local>
2018-09-15 18:28:08 -0300

    fix urlservidor

--- admin/src/server.ts
+++ admin/src/server.ts
@@ -1,5 +1,5 @@
 export let data = {
     // "urlServidor": 'http://localhost:3000', 
-    "urlServidor": 'https://ingeit.ddns.net/aguita/api/',
+    "urlServidor": 'https://ingeit.ddns.net/aguita',
     "password": "false" 
 };
