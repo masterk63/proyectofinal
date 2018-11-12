9fc281b04b6194e646aee33e54a8fac876be5df2
Ricky Bruno <ricardobruno_89@hotmail.com>
2018-11-12 20:46:00 -0300

    fix utc

--- backend/models/registro.js
+++ backend/models/registro.js
@@ -7,6 +7,7 @@ var connection = mysql.createConnection({
     user: databaseConfig.user,
     password: databaseConfig.password,
     database: databaseConfig.database,
+    timezone: 'utc'
 });
 
 var NodeGeocoder = require('node-geocoder');
